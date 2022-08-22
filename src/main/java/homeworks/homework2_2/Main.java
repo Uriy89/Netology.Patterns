4c7f4a17318ab455cffa567476565e8fe25ba9db
Юрий Аверьянов <averianovj@gmail.com>
2022-08-22 23:27:56 +0300

    home work 2.1/2.2

---
+++ src/main/java/homeworks/homework2_2/Main.java
@@ -0,0 +1,9 @@
+package homeworks.homework2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "11"));
+        System.out.println(bins.mult("10", "11"));
+    }
+}
